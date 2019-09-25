b578cc0a09d0fec810cd70b7fdeb6341730669f8
Jonathan Matsson <jmatss@kth.se>
2019-09-25 22:46:18 +0200

    Moved creation of peerId to controller and implemented more functionally to the controller

--- internal/controller.go
+++ internal/controller.go
@@ -1,97 +1,111 @@
+// TODO: if receiving error messages from "handler", remove handler from "handlers" map
+//  since it will have shut itself down
+
 package internal
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/jmatss/torc/internal/torrent"
-	. "github.com/jmatss/torc/internal/util" // "oom" util
+	. "github.com/jmatss/torc/internal/util" // "com" util
 )
 
 var (
 	PeerId string
 )
 
-func Controller(peerId string, com ComChannel) {
-	PeerId = peerId
-	// TODO: fetch torrents from disk and send to handlers when created
-	torrents := fetchTorrents()
+func Controller(clientCom ComChannel) {
+	PeerId = newPeerId()
+	var response ComMessage
+	var received ComMessage
 
 	// Spawn handlers. Every handler will be in charge of a specific torrent
-	// with the InfoHash of the torrent being used as the key in the handlers map.
-	handlers := make(map[string]ComChannel, len(torrents))
-	for _, tor := range torrents {
+	// with the InfoHash of the torrent being used as the key in the "handlers" map.
+	// The "ComChannel" in the map will be used to communicate with the handlers.
+	handlers := make(map[string]ComChannel)
+	for _, tor := range fetchTorrentsFromDisk() {
 		infoHash := string(tor.Tracker.InfoHash[:])
+		handlers[infoHash] = addTorrent(tor)
 
-		handlers[infoHash] = NewComChannel()
-		go Handler(tor, handlers[infoHash])
+		// TODO: Do this in another loop after this loop to start all handlers
+		//  before blocking and receiving the "results"
+		// Receive message from newly spawned handler and send it through to
+		// the "client" . The message will contain id "Success" or "Failure"
+		// depending on how the "start up" went fro the handler.
+		clientCom.SendCopy(handlers[infoHash].Recv())
 	}
 
-	var msg ComMessage
 	for {
-		msg = <-com.Recv
+		received = <-clientCom.RecvChan
 
-		switch msg.Id {
+		switch received.Id {
 		case Add:
-			if msg.Torrent == nil {
-				com.Respond(msg.Id, msg.InfoHash,
-					fmt.Errorf("no torrent specified when trying to \"%s\"", msg.Id.String()),
-				)
+			if received.Torrent == nil {
+				response = received
+				response.Error = fmt.Errorf("no torrent specified when trying to \"%s\"",
+					received.Id.String())
+
+				clientCom.SendCopy(response)
 			}
 
-			_, ok := torrents[string(msg.Torrent.Tracker.InfoHash[:])]
+			_, ok := handlers[string(received.Torrent.Tracker.InfoHash[:])]
 			if ok {
-				com.Respond(msg.Id, msg.InfoHash,
-					fmt.Errorf("tried to add a torrent that already exists"),
-				)
+				response = received
+				response.Error = fmt.Errorf("tried to add a torrent that already exists")
+
+				clientCom.SendCopy(response)
 			}
 
-			torrents[string(msg.Torrent.Tracker.InfoHash[:])] = msg.Torrent
-			// TODO: CODE add handler in "handlers" && go torrentHandler()
+			infoHash := string(received.Torrent.Tracker.InfoHash[:])
 
-			com.Respond(msg.Id, msg.InfoHash, nil)
+			handlers[infoHash] = addTorrent(received.Torrent)
+			// Receive response from newly added handler and sent i through to
+			// the "client."
+			clientCom.SendCopy(handlers[infoHash].Recv())
+
 		case Delete:
-			if msg.InfoHash == nil {
-				com.Respond(msg.Id, msg.InfoHash,
-					fmt.Errorf("no torrent specified when trying to \"%s\"", msg.Id.String()),
-				)
+			if received.InfoHash == nil {
+				response = received
+				response.Error = fmt.Errorf("no torrent specified when trying to \"%s\"",
+					received.Id.String())
+
+				clientCom.SendCopy(response)
 			}
 
 			// Loop through and delete all specified torrents.
 			// If a specified item doesn't exist (ex. if it already has been deleted),
 			// it will be skipped and NO error will be returned.
-			for _, infoHash := range msg.InfoHash {
-				_, ok := torrents[string(infoHash[:])]
-				if ok {
-					// TODO: CODE kill handler for specified torrent.
-					//  Might wan't to loop through and kill all other torrents
-					//  even if an error is returned from current iteration and
-					//  then let the "client" know which torrents that couldn't
-					//  be deleted, so it can decide what to do.
+			for _, infoHash := range received.InfoHash {
+				if handlerCom, ok := handlers[string(infoHash[:])]; ok {
+					clientCom.SendCopy(handlerCom.SendAndRecv(Delete, nil, nil, infoHash))
 				}
 			}
 
-			com.Respond(msg.Id, msg.InfoHash, nil)
 		case Start, Stop:
-			if msg.InfoHash == nil {
-				com.Respond(msg.Id, msg.InfoHash,
-					fmt.Errorf("no torrent specified when trying to \"%s\"", msg.Id.String()),
-				)
+			if received.InfoHash == nil {
+				response = received
+				response.Error = fmt.Errorf("no torrent specified when trying to \"%s\"",
+					received.Id.String())
+
+				clientCom.SendCopy(response)
 			}
 
 			// Loop through and start/stop all specified torrents.
-			// If a specified item doesn't exist
+			// If a specified item doesn't exist,
 			// it will be skipped and NO error will be returned.
-			for _, infoHash := range msg.InfoHash {
-				_, ok := torrents[string(infoHash[:])]
+			for _, infoHash := range received.InfoHash {
+				_, ok := handlers[string(infoHash[:])]
 				if ok {
 					// TODO: CODE start/stop
 				}
 			}
 
-			com.Respond(msg.Id, msg.InfoHash, nil)
+			clientCom.SendCopy(received)
 		case Quit:
 			for key := range handlers {
-				resp := handlers[key].Request(Quit, nil, "")
+				resp := handlers[key].SendCopyAndRecv(received)
 				if resp.Error == nil {
 					// TODO: something went wrong, do something(?) or just ignore since
 					//  the program is quiting anyways, the handler will be killed either way
@@ -103,7 +117,31 @@ func Controller(peerId string, com ComChannel) {
 	}
 }
 
+func newPeerId() string {
+	// Generate peer id. Format: -<client id(2 bytes)><version(4 bytes)>-<12 random ascii numbers>
+	// use: client id 'UT'(µTorrent) and random version for anonymity
+	peerId := make([]byte, 20)
+	peerId[0], peerId[1], peerId[2], peerId[7] = '-', 'U', 'T', '-'
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	for i := 3; i < len(peerId); i++ {
+		if i == 7 {
+			continue
+		}
+		// (ascii 48 -> 57) => (0 -> 9)
+		peerId[i] = byte(rand.Intn(10) + 48)
+	}
+
+	return string(peerId)
+}
+
 // TODO: currently only returns empty
-func fetchTorrents() map[string]*torrent.Torrent {
+func fetchTorrentsFromDisk() map[string]*torrent.Torrent {
 	return make(map[string]*torrent.Torrent, 0)
+}
+
+func addTorrent(tor *torrent.Torrent) ComChannel {
+	com := NewComChannel()
+	go Handler(tor, com, PeerId)
+	return com
 }
