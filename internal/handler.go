c3b415c904fd5f2ef673a5a15bf157d468934f86
Jonathan Matsson <jmatss@kth.se>
2019-09-25 22:48:00 +0200

    Added some more functionality in the handler

--- internal/handler.go
+++ internal/handler.go
@@ -10,11 +10,24 @@ import (
 // Handler in charge of one specific torrent.
 // Will loop forever and wait for either a message it's parent (controller)
 // or a timeout at which it should poll the tracker for new peers.
-func Handler(torrent *torrent.Torrent, com ComChannel) {
+func Handler(tor *torrent.Torrent, com ComChannel, peerId string) {
+	err := tor.Request(peerId)
+	com.Send(Add, err, nil, tor.Tracker.InfoHash)
+	if err != nil {
+		return
+	}
+
+	err = tor.Request(peerId)
+	if err != nil {
+		com.Send(Failure, err, nil, tor.Tracker.InfoHash)
+		return
+	}
+
 	// TODO: CODE start init peers
-	interval := torrent.Tracker.Interval
+	interval := tor.Tracker.Interval
 
 	if interval <= 0 {
+		//tor.R
 		//err := torrent.Request
 
 	}
@@ -23,7 +36,7 @@ func Handler(torrent *torrent.Torrent, com ComChannel) {
 	for {
 		var msg ComMessage
 		select {
-		case msg = <-com.Recv:
+		case msg = <-com.RecvChan:
 
 		case <-time.After(3 * time.Second):
 
