426e4f82ea61b79b7151574b8b0a3190cc0e998d
Jonathan Matsson <jmatss@kth.se>
2019-10-03 20:38:48 +0200

    Add logic for peer connection

--- internal/handler.go
+++ internal/handler.go
@@ -8,22 +8,29 @@ import (
 )
 
 // Handler in charge of one specific torrent.
-// Will loop forever and wait for either a message it's parent (controller)
-// or a timeout at which it should poll the tracker for new peers.
+// Will loop forever and wait for either a message from it's parent (controller)
+// , a timeout at which it should poll the tracker for new peers or an error from
+// a remote peer.
 func Handler(tor *torrent.Torrent, com ComChannel, peerId string) {
+	// Make tracker request and send back error to the controller over the ComChannel.
+	// The handler will kill itself if it isn't able to complete it's tracker request.
 	err := tor.Request(peerId)
 	com.Send(Add, err, nil, tor.Tracker.InfoHash)
 	if err != nil {
 		return
 	}
 
-	err = tor.Request(peerId)
-	if err != nil {
-		com.Send(Failure, err, nil, tor.Tracker.InfoHash)
-		return
+	// TODO: Maybe add an extra error message at the end of the loop
+	//  if all peers fail.
+	for _, peer := range tor.Tracker.Peers {
+		conn, err := peer.Handshake(peerId, tor.Tracker.InfoHash)
+		if err != nil {
+			com.Send(Failure, err, nil, tor.Tracker.InfoHash)
+		} else {
+			peer.Connection = conn
+		}
 	}
 
-	// TODO: CODE start init peers
 	interval := tor.Tracker.Interval
 
 	if interval <= 0 {
