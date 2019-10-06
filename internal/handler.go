c6cfa401cdb66572fbede326ffbbea80e16cee55
Jonathan Matsson <jmatss@kth.se>
2019-10-06 14:03:57 +0200

    Work on handler

--- internal/handler.go
+++ internal/handler.go
@@ -7,11 +7,18 @@ import (
 	. "github.com/jmatss/torc/internal/util"
 )
 
+const (
+	MaxRetryCount = 5
+)
+
 // Handler in charge of one specific torrent.
 // Will loop forever and wait for either a message from it's parent (controller)
 // , a timeout at which it should poll the tracker for new peers or an error from
 // a remote peer.
+// TODO: setup so that other peers can connect to this handler
 func Handler(tor *torrent.Torrent, com ComChannel, peerId string) {
+	retryCount := 0
+
 	// Make tracker request and send back error to the controller over the ComChannel.
 	// The handler will kill itself if it isn't able to complete it's tracker request.
 	err := tor.Request(peerId)
@@ -33,20 +40,26 @@ func Handler(tor *torrent.Torrent, com ComChannel, peerId string) {
 
 	interval := tor.Tracker.Interval
 
-	if interval <= 0 {
-		//tor.R
-		//err := torrent.Request
-
-	}
-
 	// TODO: implement
 	for {
 		var msg ComMessage
+
 		select {
 		case msg = <-com.RecvChan:
 
-		case <-time.After(3 * time.Second):
-
+		case <-time.After(time.Duration(interval) * time.Second):
+			// TODO: implement some sort of retry functionality so that
+			//  it just doesn't kill itself after
+			err := tor.Request(peerId)
+			if err != nil {
+				retryCount++
+				if retryCount >= MaxRetryCount {
+					com.Send(Failure, err, nil, tor.Tracker.InfoHash)
+					if err != nil {
+						return
+					}
+				}
+			}
 		}
 	}
 
