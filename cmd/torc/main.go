de03b2ea8fb1d51c92be07957cd227f06a017a13
Jonathan Matsson <jmatss@kth.se>
2019-10-06 20:05:04 +0200

    Add basic main/client logic

--- cmd/torc/main.go
+++ cmd/torc/main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/jmatss/torc/internal"
 )
 
 func main() {
-	args := os.Args
-	if len(args) != 2 {
-		fmt.Fprintf(os.Stderr, "Enter torrent file as argument")
-		os.Exit(0)
-	}
-
-	internal.Run(args[1])
+	internal.Run()
 }
