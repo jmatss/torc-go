3de02e1d8b9ff3ef3c7c6ce6af51ccffe80fa378
Jonathan Matsson <jmatss@kth.se>
2019-10-10 21:36:52 +0200

    Add logic for peerhandler.go

--- internal/peerhandler.go
+++ internal/peerhandler.go
@@ -3,19 +3,11 @@ package internal
 import (
 	"encoding/binary"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+	"log"
 
 	"github.com/jmatss/torc/internal/torrent"
 	"github.com/jmatss/torc/internal/util/com"
-)
-
-const (
-	// 2^15 max according to unofficial specs.
-	// See https://wiki.theory.org/index.php/BitTorrentSpecification#Messages
-	// (section "request")
-	MaxRequestLength = 1 << 15
+	"github.com/jmatss/torc/internal/util/cons"
 )
 
 type remoteDTO struct {
@@ -31,26 +23,49 @@ func PeerHandler(comTorrentHandler com.Channel, peer *torrent.Peer, tor *torrent
 
 	// Peer handshake. This handler will kill itself if it isn't able to
 	// complete the handshake.
-	conn, err := peer.Handshake(PeerId, tor.Tracker.InfoHash)
+	conn, err := peer.Handshake(cons.PeerId, tor.Tracker.InfoHash)
 	if err != nil {
-		comTorrentHandler.SendParent(com.TotalFailure, err, nil, childId)
+		log.Printf("PeerHandler handshake failure!: %w", err)
+		comTorrentHandler.SendParent(com.TotalFailure, nil, err, nil, childId)
 		return
 	}
 	peer.Connection = conn
-	defer peer.Connection.Close()
-	comTorrentHandler.SendParent(com.Success, nil, nil, childId)
+	defer func() {
+		peer.Connection.Close()
+		log.Printf("PeerHandler exiting")
+	}()
+
+	comTorrentHandler.SendParent(com.Success, nil, nil, nil, childId)
 	comTorrentHandler.AddChild(childId)
 	defer comTorrentHandler.RemoveChild(childId)
+
+	//peer.SendData(torrent.Bitfield, tor.Tracker.BitFieldHave)
+	// Start by un choking remote peer and send that this client is interested
+	// TODO: these state doesn't currently change after this point, CODE
+	peer.Send(torrent.Interested)
+	peer.Send(torrent.UnChoke)
 
 	// RemoteBitField initialized to all zeros
 	bitFieldLength := ((len(tor.Info.Pieces) / 20) / 8) + 1
 	peer.RemoteBitField = make([]byte, bitFieldLength)
 
+	/*
+		Spawn a downloader that requests data from the remote peer.
+		This PeerHandler will receive the data from the remote peer
+		and forward it to the downloader via the downloadChannel.
+	*/
+	downloadChannel := make(chan remoteDTO, com.ChanSize)
+	go downloader(comTorrentHandler, downloadChannel, tor, peer)
+
+	/*
+		Spawn a go process that receives data from the remote peer
+		and puts it into the "readChannel".
+	*/
 	// TODO: modify buffer size
 	// TODO: kill this go process when the PeerHandler exits
 	readChannel := make(chan remoteDTO, com.ChanSize)
 	go func() {
-		for true {
+		for {
 			id, data, err := peer.Recv()
 			readChannel <- remoteDTO{id, data, err}
 
@@ -58,244 +73,242 @@ func PeerHandler(comTorrentHandler com.Channel, peer *torrent.Peer, tor *torrent
 			// The receiver of the message over the "readChannel" can check and see that
 			// "Err" is set, and figure out that this go process is dead.
 			if err != nil {
+				log.Printf("read func exiting, err: %v", err)
 				return
 			}
 		}
 	}()
 
-	select {
-	case received := <-comTorrentHandler.GetChildChannel(childId):
-		/*
-			Received message from "torrentHandler"/parent.
-		*/
-		switch received.Id {
-		case com.Quit:
-			// TODO: Kill internal "readChannel" go process
-			return
-		}
+	for {
+		select {
+		case received := <-comTorrentHandler.GetChildChannel(childId):
+			/*
+				Received message from "torrentHandler"/parent.
+			*/
+			switch received.Id {
+			case com.Have:
+				pieceIndex := binary.BigEndian.Uint32(received.Data)
+				peer.Send(torrent.Have, int(pieceIndex))
 
-	case received := <-readChannel:
-		/*
-			Received message from remote peer.
-		*/
-		// Kills itself if it receives and error
-		if received.Err != nil {
-			comTorrentHandler.SendParent(com.TotalFailure, err, nil, childId)
-			return
-		}
+			case com.Quit:
+				// TODO: Kill internal "readChannel" go process & downloader
+				return
+			}
 
-		switch received.Id {
-		case torrent.KeepAlive:
-			// TODO: do something?
-		case torrent.Choke:
-			peer.PeerChoking = true
-		case torrent.UnChoke:
-			peer.PeerChoking = false
-		case torrent.Interested:
-			peer.PeerInterested = true
-		case torrent.NotInterested:
-			peer.PeerInterested = false
-		case torrent.Have:
-			// Remote peer indicates that it has just received the piece with the index "pieceIndex".
-			// Update the "RemoteBitField" in this peer struct by OR:ing in a 1 at the correct index.
-			pieceIndex := binary.BigEndian.Uint32(received.Data)
-			byteShift := pieceIndex / 8
-			bitShift := 8 - (pieceIndex % 8) // bits are stored in "reverse"
-			if int(byteShift) > len(peer.RemoteBitField) {
+		case received := <-readChannel:
+			/*
+				Received message from remote peer.
+			*/
+			// Kills itself if it receives an error
+			if received.Err != nil {
+				comTorrentHandler.SendParent(com.TotalFailure, nil, err, nil, childId)
+				return
+			}
+
+			switch received.Id {
+			case torrent.KeepAlive:
+				// TODO: do something?
+			case torrent.Choke:
+				peer.PeerChoking = true
+				downloadChannel <- received
+			case torrent.UnChoke:
+				peer.PeerChoking = false
+				downloadChannel <- received
+			case torrent.Interested:
+				peer.PeerInterested = true
+				downloadChannel <- received
+			case torrent.NotInterested:
+				peer.PeerInterested = false
+				downloadChannel <- received
+			case torrent.Have:
+				// Remote peer indicates that it has just received the piece with the index "pieceIndex".
+				// Update the "RemoteBitField" in this peer struct by OR:ing in a 1 at the correct index.
+				pieceIndex := binary.BigEndian.Uint32(received.Data)
+				byteShift := pieceIndex / 8
+				bitShift := 8 - (pieceIndex % 8) // bits are stored in "reverse"
+				if int(byteShift) > len(peer.RemoteBitField) {
+					comTorrentHandler.SendParent(
+						com.TotalFailure,
+						nil,
+						fmt.Errorf("the remote peer has specified a piece index that is to big to "+
+							"fit in it's bitfield"),
+						nil,
+						childId,
+					)
+					return
+				}
+
+				// TODO: might deadlock if the operations fails
+				peer.Lock()
+				peer.RemoteBitField[byteShift] |= 1 << bitShift
+				peer.Unlock()
+
+			case torrent.Bitfield:
+				// If correct length, assume correct bitfield. Update local to match remote.
+				if len(received.Data) == len(peer.RemoteBitField) {
+					peer.Lock()
+					peer.RemoteBitField = received.Data
+					peer.Unlock()
+				}
+
+			case torrent.Request:
+				// TODO: do in another go process or another file/function
+				requestedData, err := tor.ReadData(received.Data)
+				if err != nil {
+					// TODO: some sort of logging or feedback of this failure.
+					//  Not so important since it most likely the remote peer that has an error.
+					break
+				}
+
+				// Send requested data to remote peer
+				if err := peer.SendData(torrent.Piece, requestedData); err != nil {
+					// TODO: some sort of logging or feedback of this failure.
+				}
+
+			case torrent.Piece:
+				downloadChannel <- received
+
+				// TODO: some sort of logging or feedback of this success.
+			case torrent.Cancel:
+				// TODO: Nothing to do atm, might need to add functionality later
+			default:
 				comTorrentHandler.SendParent(
 					com.TotalFailure,
-					fmt.Errorf("the remote peer has specified a piece index that is to big to "+
-						"fit in it's bitfield"),
+					nil,
+					fmt.Errorf("unexpected message id \"%d\"", received.Id),
 					nil,
 					childId,
 				)
 				return
 			}
-			peer.RemoteBitField[byteShift] |= 1 << bitShift
+		}
+	}
+}
 
-		case torrent.Bitfield:
-			// If correct length, assume correct bitfield. Update local to match remote.
-			if len(received.Data) == len(peer.RemoteBitField) {
-				peer.RemoteBitField = received.Data
-			}
-
-		case torrent.Request:
-			// TODO: do in another go process or another file/function
-			requestedData, err := getData(tor, received.Data)
-			if err != nil {
-				// TODO: some sort of logging or feedback of this failure.
-				//  Not so important since it most likely the remote peer that has an error.
-				break
-			}
-
-			// Send request data to remote peer
-			if err := peer.SendData(torrent.Piece, requestedData); err != nil {
-				// TODO: some sort of logging or feedback of this failure.
-			}
-
-		case torrent.Piece:
-			if err := setData(tor, received.Data); err != nil {
-				comTorrentHandler.SendParent(
-					com.Failure,
-					err,
-					tor,
-					childId,
-				)
-			}
-
-			// TODO: some sort of logging or feedback of this success.
-		case torrent.Cancel:
-			// TODO: Nothing to do atm, might need to add functionality later
-		default:
-			comTorrentHandler.SendParent(
-				com.TotalFailure,
-				fmt.Errorf("unexpected message id \"%d\"", received.Id),
-				nil,
-				childId,
-			)
+// Download pieces from this remote peer.
+func downloader(
+	comTorrentHandler com.Channel,
+	receiveChan chan remoteDTO,
+	t *torrent.Torrent,
+	p *torrent.Peer,
+) {
+	for {
+		pieceIndex, err := downloadPiece(receiveChan, t, p)
+		if err != nil {
 			return
 		}
+
+		log.Printf("piece %d downloaded", pieceIndex)
+
+		// Send have message to torrentHandler to let it now that a new piece is downloaded
+		// and a Have message can be sent to all peers.
+		data := make([]byte, 4)
+		binary.BigEndian.PutUint32(data, uint32(pieceIndex))
+		comTorrentHandler.SendParent(com.Have, data, nil, nil, "")
 	}
 }
 
-// TODO: add more argument validation so that it doesn't request data outside the piece etc.
-func getData(tor *torrent.Torrent, request []byte) ([]byte, error) {
-	pieceIndex := binary.BigEndian.Uint32(request[:4])
-	begin := binary.BigEndian.Uint32(request[4:8])
-	length := binary.BigEndian.Uint32(request[8:])
-
-	if pieceIndex <= 0 || pieceIndex >= uint32(len(tor.Info.Pieces)/20) {
-		return nil, fmt.Errorf("piece index is incorrect: "+
-			"expected: %d >= pieceIndex >= 0, got: %d", len(tor.Info.Pieces)/20, pieceIndex)
-	}
-	if begin <= 0 || begin >= uint32(tor.Info.PieceLength) {
-		return nil, fmt.Errorf("begin is incorrect: "+
-			"expected: %d >= pieceIndex >= 0, got: %d", tor.Info.PieceLength, begin)
-	}
-	if length > MaxRequestLength {
-		return nil, fmt.Errorf("length is over MaxRequestLength: "+
-			"expected: <%d, got: %d", MaxRequestLength, length)
+func downloadPiece(receiveChan chan remoteDTO, t *torrent.Torrent, p *torrent.Peer) (int, error) {
+	pieceIndex, err := findFreePieceIndex(t, p)
+	if err != nil {
+		return -1, err
 	}
 
-	// The "real" index of the whole "byte stream" where the remote peer wants
-	// to start reading data at. The remote peer wants "length" bytes starting
-	// from this index.
-	requestIndex := int64(pieceIndex)*tor.Info.PieceLength + int64(begin)
-	data := make([]byte, length)
-	dataBuffer := data
+	// The last piece will have a size less than t.Info.PieceLength, prevent overflow.
+	// If true:
+	//  this piece is the last piece, will have a size of less than "piece length"
+	pieceLength := t.Info.PieceLength
+	amountOfPieces := len(t.Info.Pieces) / 20
+	if pieceIndex == amountOfPieces-1 {
+		pieceLength = t.Tracker.Left
+	}
 
-	for _, file := range tor.Info.Files {
-		// Skip files that have an index less than the requested start index.
-		if requestIndex < file.Index {
-			continue
-		}
+	// Update BitFields in Tracker according to how this download operation went.
+	// If error:
+	//  clear BitFieldDownloading so that this piece can be re-downloaded.
+	// Else:
+	//  piece downloaded, set BitFieldHave to indicate that client has downloaded the piece.
+	defer func() {
+		t.Tracker.Lock()
+		defer t.Tracker.Unlock()
 
-		path := filepath.FromSlash(DownloadPath + "/" + strings.Join(file.Path, "/"))
-		f, err := os.OpenFile(path, os.O_RDONLY, 0444)
+		byteIndex := pieceIndex / 8
+		bitIndex := pieceIndex % 8
 		if err != nil {
-			return nil, fmt.Errorf("unable to open file %s: %w", path, err)
-		}
-
-		_, err = f.Seek(requestIndex-file.Index, 0)
-		if err != nil {
-			f.Close()
-			return nil, fmt.Errorf("unable to seek in file %s: %w", path, err)
-		}
-
-		n, err := f.Read(dataBuffer)
-		if err != nil {
-			f.Close()
-			return nil, fmt.Errorf("unable to read data from file %s: %w", path, err)
-		}
-		f.Close()
-
-		/*
-			If true:
-			  This file didn't contain all the requested data.
-			  Continue reading data from the next file.
-			  Change "offset"/start for byte slice so that the read data from this file
-			  doesn't get overwritten.
-			Else:
-			  All data read, break and send data to remote peer.
-		*/
-		if n < int(length) {
-			dataBuffer = dataBuffer[n:]
+			t.Tracker.BitFieldDownloading[byteIndex] &^= 1 << (7 - uint(bitIndex))
 		} else {
-			break
+			t.Tracker.BitFieldHave[byteIndex] |= 1 << (7 - uint(bitIndex))
 		}
+	}()
+
+	// Read the whole piece one request at a time and write to disk.
+	// TODO: More error checking. length of received data etc.
+	// TODO: Add timeout so it doesn't hang if it doesn't get an answer.
+	begin := 0
+	var received remoteDTO
+	for int64(begin) < pieceLength {
+		// Prevent overflow of the last "block".
+		// TODO: fix possibility for overflow (in64 -> int)
+		requestLength := torrent.MaxRequestLength
+		if int64(begin+torrent.MaxRequestLength) > pieceLength {
+			requestLength = int(pieceLength) - begin
+		}
+
+		for {
+			// Loop forever until the remote peer un chokes this client and this client
+			// receives a "piece" message
+			if p.PeerChoking {
+				received = <-receiveChan
+			} else {
+				p.Send(torrent.Request, pieceIndex, begin, requestLength)
+				received = <-receiveChan
+				if received.Id == torrent.Piece {
+					break
+				}
+			}
+		}
+
+		_, err := t.WriteData(received.Data)
+		if err != nil {
+			log.Printf("error writing to file: %v", err)
+		}
+
+		begin += torrent.MaxRequestLength
 	}
 
-	return data, nil
+	return pieceIndex, nil
 }
 
-func setData(tor *torrent.Torrent, piece []byte) error {
-	pieceIndex := binary.BigEndian.Uint32(piece[:4])
-	begin := binary.BigEndian.Uint32(piece[4:8])
-	data := piece[8:]
+// Returns a free piece that needs to be downloaded and this remote peer has.
+// (or an error)
+func findFreePieceIndex(t *torrent.Torrent, p *torrent.Peer) (int, error) {
+	t.Tracker.Lock()
+	defer t.Tracker.Unlock()
+	p.Lock()
+	defer p.Unlock()
 
-	if pieceIndex <= 0 || pieceIndex >= uint32(len(tor.Info.Pieces)/20) {
-		return fmt.Errorf("piece index is incorrect: "+
-			"expected: %d >= pieceIndex >= 0, got: %d", len(tor.Info.Pieces)/20, pieceIndex)
-	}
-	if begin <= 0 || begin >= uint32(tor.Info.PieceLength) {
-		return fmt.Errorf("begin is incorrect: "+
-			"expected: %d >= pieceIndex >= 0, got: %d", tor.Info.PieceLength, begin)
-	}
-	if len(data) > MaxRequestLength {
-		return fmt.Errorf("length is over MaxRequestLength: "+
-			"expected: <%d, got: %d", MaxRequestLength, len(data))
-	}
+	amountOfPieces := len(t.Info.Pieces) / 20
 
-	// The "real" index of the whole "byte stream".
-	requestIndex := int64(pieceIndex)*tor.Info.PieceLength + int64(begin)
-	var off int64 = 0
+	for i := 0; i < amountOfPieces; i++ {
+		byteIndex := i / 8
+		bitIndex := i % 8
+		taken := int(t.Tracker.BitFieldDownloading[byteIndex]) & (1 << (7 - uint(bitIndex)))
 
-	for _, file := range tor.Info.Files {
-		// Skip files that have an index less than the requested start index.
-		if requestIndex < file.Index {
-			continue
-		}
-
-		path := filepath.FromSlash(DownloadPath + "/" + strings.Join(file.Path, "/"))
-		f, err := os.OpenFile(path, os.O_WRONLY, 0444)
-		if err != nil {
-			return fmt.Errorf("unable to open file %s: %w", path, err)
-		}
-
-		seekPos := requestIndex - file.Index
-		_, err = f.Seek(seekPos, 0)
-		if err != nil {
-			f.Close()
-			return fmt.Errorf("unable to seek in file %s: %w", path, err)
-		}
-
-		/*
-			If true:
-			  This write will not fill the whole file.
-			  Limit the amount of bytes writen to not get a índex out of bounds
-			  when accessing the data in the "data" buffer.
-			Else:
-			  There are more data to write then there are bytes left in this file.
-			  Will continue writing the rest of the data in the next file.
-		*/
-		amountToWrite := file.Length - seekPos
-		if amountToWrite > int64(len(data)) {
-			amountToWrite = int64(len(data))
-		}
-
-		n, err := f.Write(data[off : off+amountToWrite])
-		if err != nil {
-			f.Close()
-			return fmt.Errorf("unable to write data to file %s: %w", path, err)
-		}
-		f.Close()
-
-		off += int64(n)
-		// All the data have been written to files, break
-		if off >= int64(len(data)) {
-			break
+		// If taken < 1:
+		//  this piece is free to download.
+		//  If (this remote peer has that piece):
+		//    download it
+		//  Else:
+		//    continue to loop through and look for a free piece that this remote peer has.
+		// Else (taken >= 1):
+		//  this piece is taken i.e. is downloaded or is being downloaded from other peer.
+		//  continue to loop through and look for a free piece that this remote peer has.
+		if taken < 1 {
+			// Set the piece to "taken"
+			t.Tracker.BitFieldDownloading[byteIndex] |= 1 << (7 - uint(bitIndex))
+			return i, nil
 		}
 	}
 
-	return nil
+	return -1, fmt.Errorf("unable to find a free piece for this peer")
 }
